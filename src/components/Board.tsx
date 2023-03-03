8ca0f4e1ec7c7d2cc65efc556746c1d718c4469d
dominic Roof <dominiclroof@gmail.com>
2023-03-04 01:10:25 +0800

    optimise code

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
   getMines,
   getFlags,
@@ -27,117 +27,134 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
     setBoardData(data);
   }, [height, width, mines]);
 
-  // reveals the whole board
-  function revealBoard(
-    boardData: CellType[][],
-    setBoardData: React.Dispatch<React.SetStateAction<CellType[][]>>
-  ) {
-    const updatedData = [...boardData];
-    updatedData.map((datarow) => {
-      datarow.map((dataitem) => {
-        dataitem.isRevealed = true;
+  const revealBoard = useCallback(
+    (
+      boardData: CellType[][],
+      setBoardData: React.Dispatch<React.SetStateAction<CellType[][]>>
+    ): void => {
+      const updatedData = [...boardData];
+      updatedData.map((datarow) => {
+        datarow.map((dataitem) => {
+          dataitem.isRevealed = true;
+        });
       });
-    });
-    setBoardData(updatedData);
-  }
+      setBoardData(updatedData);
+    },
+    []
+  );
 
-  function revealEmpty(x: number, y: number, data: CellType[][]) {
-    let area = traverseBoard(x, y, data, width, height);
-    area.forEach((value) => {
-      if (
-        !value.isFlagged &&
-        !value.isRevealed &&
-        (value.isEmpty || !value.isMine)
-      ) {
-        data[value.x][value.y].isRevealed = true;
-        if (value.isEmpty) {
-          revealEmpty(value.x, value.y, data);
+  const revealEmpty = useCallback(
+    (x: number, y: number, data: CellType[][]) => {
+      let area = traverseBoard(x, y, data, width, height);
+      area.forEach((value) => {
+        if (
+          !value.isFlagged &&
+          !value.isRevealed &&
+          (value.isEmpty || !value.isMine)
+        ) {
+          data[value.x][value.y].isRevealed = true;
+          if (value.isEmpty) {
+            revealEmpty(value.x, value.y, data);
+          }
         }
+      });
+      return data;
+    },
+    [width, height]
+  );
+
+  const handleCellClick = useCallback(
+    (x: number, y: number) => {
+      // check if revealed. return if true.
+      if (boardData[x][y].isRevealed || boardData[x][y].isFlagged) return null;
+
+      // check if mine. game over if true
+      if (boardData[x][y].isMine) {
+        setGameStatus("You Lost.");
+        revealBoard(boardData, setBoardData);
+        alert("game over");
       }
-    });
-    return data;
-  }
 
-  function handleCellClick(x: number, y: number) {
-    // check if revealed. return if true.
-    if (boardData[x][y].isRevealed || boardData[x][y].isFlagged) return null;
-
-    // check if mine. game over if true
-    if (boardData[x][y].isMine) {
-      setGameStatus("You Lost.");
-      revealBoard(boardData, setBoardData);
-      alert("game over");
-    }
-
-    const updatedData = boardData.map((datarow) =>
-      datarow.map((dataitem) => ({ ...dataitem }))
-    );
-    updatedData[x][y].isFlagged = false;
-    updatedData[x][y].isRevealed = true;
-
-    if (updatedData[x][y].isEmpty) {
-      revealEmpty(x, y, updatedData);
-    }
-
-    if (getHidden(updatedData).length === mines) {
-      setMineCount(0);
-      setGameStatus("You Win.");
-      revealBoard(boardData, setBoardData);
-      alert("You Win");
-    }
-
-    setBoardData(updatedData);
-    setMineCount(mines - getFlags(updatedData).length);
-  }
-
-  function handleContextMenu(e: React.MouseEvent, x: number, y: number) {
-    e.preventDefault();
-    let updatedData = boardData;
-    let mines = mineCount;
-
-    // check if already revealed
-    if (updatedData[x][y].isRevealed) return;
-
-    if (updatedData[x][y].isFlagged) {
+      const updatedData = boardData.map((datarow) =>
+        datarow.map((dataitem) => ({ ...dataitem }))
+      );
       updatedData[x][y].isFlagged = false;
-      mines++;
-    } else {
-      updatedData[x][y].isFlagged = true;
-      mines--;
-    }
+      updatedData[x][y].isRevealed = true;
 
-    if (mines === 0) {
-      const mineArray = getMines(updatedData);
-      const FlagArray = getFlags(updatedData);
-      if (JSON.stringify(mineArray) === JSON.stringify(FlagArray)) {
+      if (updatedData[x][y].isEmpty) {
+        revealEmpty(x, y, updatedData);
+      }
+
+      if (getHidden(updatedData).length === mines) {
         setMineCount(0);
         setGameStatus("You Win.");
         revealBoard(boardData, setBoardData);
         alert("You Win");
       }
-    }
 
-    setBoardData(updatedData);
-    setMineCount(mines);
-  }
+      setBoardData(updatedData);
+      setMineCount(mines - getFlags(updatedData).length);
+    },
+    [boardData, mines, revealEmpty]
+  );
 
-  const renderBoard = (data: CellType[][]): JSX.Element[] =>
-    Array.from(data, (datarow, rowIndex) => (
-      <div key={rowIndex}>
-        {datarow.map((dataitem, colIndex) => (
-          <div key={`${rowIndex}-${colIndex}`}>
-            <Cell
-              onClick={() => handleCellClick(dataitem.x, dataitem.y)}
-              cMenu={(e: React.MouseEvent) =>
-                handleContextMenu(e, dataitem.x, dataitem.y)
-              }
-              value={dataitem}
-            />
-            {colIndex === datarow.length - 1 ? <div className="clear" /> : null}
+  const handleContextMenu = useCallback(
+    (e: React.MouseEvent, x: number, y: number) => {
+      e.preventDefault();
+      let updatedData = boardData;
+      let mines = mineCount;
+
+      // check if already revealed
+      if (updatedData[x][y].isRevealed) return;
+
+      if (updatedData[x][y].isFlagged) {
+        updatedData[x][y].isFlagged = false;
+        mines++;
+      } else {
+        updatedData[x][y].isFlagged = true;
+        mines--;
+      }
+
+      if (mines === 0) {
+        const mineArray = getMines(updatedData);
+        const FlagArray = getFlags(updatedData);
+        if (JSON.stringify(mineArray) === JSON.stringify(FlagArray)) {
+          setMineCount(0);
+          setGameStatus("You Win.");
+          revealBoard(boardData, setBoardData);
+          alert("You Win");
+        }
+      }
+
+      setBoardData(updatedData);
+      setMineCount(mines);
+    },
+    [boardData, mineCount, revealBoard, setBoardData, setGameStatus]
+  );
+
+  const renderBoard = React.useMemo(
+    () =>
+      (data: CellType[][]): JSX.Element[] =>
+        Array.from(data, (datarow, rowIndex) => (
+          <div key={rowIndex}>
+            {datarow.map((dataitem, colIndex) => (
+              <div key={`${rowIndex}-${colIndex}`}>
+                <Cell
+                  onClick={() => handleCellClick(dataitem.x, dataitem.y)}
+                  cMenu={(e: React.MouseEvent) =>
+                    handleContextMenu(e, dataitem.x, dataitem.y)
+                  }
+                  value={dataitem}
+                />
+                {colIndex === datarow.length - 1 ? (
+                  <div className="clear" />
+                ) : null}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    ));
+        )),
+    [handleCellClick, handleContextMenu]
+  );
 
   return (
     <div className="board">
