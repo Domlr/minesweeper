3a7be34deafa4de3124d9b615b221249102f6d9c
dominic Roof <dominiclroof@gmail.com>
2023-03-04 18:18:41 +0800

    change number back

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,7 +7,7 @@ type GameProps = {};
 export default function Game(props: GameProps) {
   const [height, setHeight] = useState<number>(8);
   const [width, setWidth] = useState<number>(8);
-  const [mines, setMines] = useState<number>(1);
+  const [mines, setMines] = useState<number>(10);
 
   return (
     <div className="game">
