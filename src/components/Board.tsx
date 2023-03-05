403a78eb08d776b0b7ac9ea43dc272d71d8c423a
dominic Roof <dominiclroof@gmail.com>
2023-03-05 23:55:06 +0800

    update comments and make updateddata spread data

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -31,12 +31,16 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
   const [firstClick, setFirstClick] = useState(true);
   const [timer, setTimer] = useState<number>(0);
 
+  // this will check if the mines are greater than the cells and set the status
+  // to invalid params, however this wasn't to be an issue as I would use
+  // select difficulties to lessen the user error
   useEffect(() => {
     if (mines >= height * width) {
       return setGameStatus(GameStatus.InvalidParams);
     }
   }, [height, width, mines, gameStatus]);
 
+  //we set this to initialize the board data with the props passed in
   useEffect(() => {
     const data = initBoardData(height, width, mines);
     setBoardData(data);
@@ -44,18 +48,23 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
+    //this will start the timer is it is the first click and the game is running
     if (!firstClick && gameStatus === GameStatus.InProgress) {
       intervalId = setInterval(() => {
         setTimer((time) => time + 1);
       }, 1000);
     }
+    //this will stop it in any other condition, which is updated by the gamestatus
     return () => clearInterval(intervalId);
   }, [firstClick, gameStatus]);
 
   useEffect(() => {
+    //if it is the first click it will set the game to inprogress
     !firstClick && setGameStatus(GameStatus.InProgress);
   }, [firstClick]);
 
+  //this will reveal all the cells on the board at the end of the game
+  //it takes the board, updates each row and each sell to true and sets the board data
   const revealBoard = useCallback(
     (
       boardData: CellType[][],
@@ -72,6 +81,7 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
     []
   );
 
+  //this is used to reset the game to it's initial state, to start a new game
   const resetGame = useCallback(() => {
     const data = initBoardData(height, width, mines);
     setBoardData(data);
@@ -81,6 +91,10 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
     setTimer(0);
   }, [height, width, mines]);
 
+  //this uses recursive function to go through the cell of x and y and get the point boundaries.
+  //then with the array it returns it loops through each and reveals any that are empty
+  //then if the value is empty of that cell, it will call it again and do the same check
+  //until all conditions are met for every empty cell adjacent
   const revealEmpty = useCallback(
     (x: number, y: number, data: CellType[][]) => {
       let area = pointBoundaries(x, y, data, width, height);
@@ -100,11 +114,11 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
     },
     [width, height]
   );
-
+  //this will handle the operations of a cell when clicked
   const handleCellClick = useCallback(
     (x: number, y: number) => {
       let updatedData = [...boardData];
-      // Plant mines on first click
+      // Plant mines on first click, so you won't lose on first click
       if (firstClick) {
         const updatedDataWithMines = initBoardData(
           height,
@@ -121,7 +135,7 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
       if (updatedData[x][y].isRevealed || updatedData[x][y].isFlagged)
         return null;
 
-      // check if mine. game over if true
+      // this will check and make you lost
       if (updatedData[x][y].isMine) {
         setGameStatus(GameStatus.Lose);
         revealBoard(boardData, setBoardData);
@@ -130,10 +144,12 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
       updatedData[x][y].isFlagged = false;
       updatedData[x][y].isRevealed = true;
 
+      //we check the cell to see if it's empty, then use the recursive function to check other cells
       if (updatedData[x][y].isEmpty) {
         revealEmpty(x, y, updatedData);
       }
 
+      //this will check to see if the hidden tiles are equal to how many mines, and set to win
       if (getHidden(updatedData).length === mines) {
         setMineCount(0);
         setGameStatus(GameStatus.Win);
@@ -146,16 +162,17 @@ const Board: React.FC<BoardProps> = ({ height, width, mines }) => {
     [boardData, firstClick, height, mines, revealEmpty, revealBoard, width]
   );
 
+  //handle the rightclick to flag a cell
   const handleContextMenu = useCallback(
     (e: React.MouseEvent, x: number, y: number) => {
       e.preventDefault();
 
-      // Check if first click has been made
+      // Check if first click has been made, won't let you click before game started
       if (firstClick) {
         return;
       }
 
-      let updatedData = boardData;
+      let updatedData = [...boardData];
       let mines = mineCount;
 
       // check if already revealed
