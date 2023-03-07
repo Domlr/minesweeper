26927df3841cd38b986ef4c5567364b3ee12f648
dominic Roof <dominiclroof@gmail.com>
2023-03-07 23:53:34 +0800

    increase mines

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,7 +8,7 @@ export default function Game(props: GameProps) {
   //keeping in as planning to add a menu to change these with difficulty settings
   const [height, setHeight] = useState<number>(8);
   const [width, setWidth] = useState<number>(8);
-  const [mines, setMines] = useState<number>(1);
+  const [mines, setMines] = useState<number>(10);
 
   return (
     <div className="game">
